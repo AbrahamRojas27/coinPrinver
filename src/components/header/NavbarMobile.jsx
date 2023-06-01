ddce6ddbb9b6c2f8fa66f52125fcf8a8e76beb88
abrahamRojas27 <abraham.art2712@gmail.com>
2023-06-01 15:28:10 -0400

    commit

--- src/components/header/NavbarMobile.jsx
+++ src/components/header/NavbarMobile.jsx
@@ -35,6 +35,9 @@ function NavbarMobile(){
             <li>
               <a href="https://coinpinver.com/Subastaexchange/noticias" className="nav-item">Señales</a>
             </li>
+            <li>
+              <a href="https://tokencpge.coinpinver.com/" className="token-button">Compra nuestro CPGE</a>
+            </li>
         </ul>
        </nav>
     )
